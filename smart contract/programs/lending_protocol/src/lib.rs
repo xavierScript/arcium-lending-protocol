cfca0c35e5fe153766107189504f07618703ebe2
xavierscript <david111francisco@gmail.com>
2025-11-19 08:30:38 +0100

    an error remaining to be fixed

--- smart contract/programs/lending_protocol/src/lib.rs
+++ smart contract/programs/lending_protocol/src/lib.rs
@@ -103,12 +103,12 @@ pub mod lending_protocol {
 
     // Callback after health check computation
     #[arcium_callback(encrypted_ix = "check_health_factor")]
-    pub fn health_check_callback(
-        ctx: Context<HealthCheckCallback>,
-        output: ComputationOutputs<HealthCheckOutput>,
+    pub fn check_health_factor_callback(
+        ctx: Context<CheckHealthFactorCallback>,
+        output: ComputationOutputs<CheckHealthFactorOutput>,
     ) -> Result<()> {
         let result = match output {
-            ComputationOutputs::Success(HealthCheckOutput { field_0 }) => field_0,
+            ComputationOutputs::Success(CheckHealthFactorOutput { field_0 }) => field_0,
             _ => return Err(ErrorCode::AbortedComputation.into()),
         };
 
@@ -179,12 +179,12 @@ pub mod lending_protocol {
 
     // Callback after liquidation check
     #[arcium_callback(encrypted_ix = "check_liquidation")]
-    pub fn liquidation_callback(
-        ctx: Context<LiquidationCallback>,
-        output: ComputationOutputs<LiquidationOutput>,
+    pub fn check_liquidation_callback(
+        ctx: Context<CheckLiquidationCallback>,
+        output: ComputationOutputs<CheckLiquidationOutput>,
     ) -> Result<()> {
         let result = match output {
-            ComputationOutputs::Success(LiquidationOutput { field_0 }) => field_0,
+            ComputationOutputs::Success(CheckLiquidationOutput { field_0 }) => field_0,
             _ => return Err(ErrorCode::AbortedComputation.into()),
         };
 
@@ -197,106 +197,14 @@ pub mod lending_protocol {
     }
 }
 
-#[queue_computation_accounts("add_together", payer)]
-#[derive(Accounts)]
-#[instruction(computation_offset: u64)]
-pub struct AddTogether<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-    #[account(
-        init_if_needed,
-        space = 9,
-        payer = payer,
-        seeds = [&SIGN_PDA_SEED],
-        bump,
-        address = derive_sign_pda!(),
-    )]
-    pub sign_pda_account: Account<'info, SignerAccount>,
-    #[account(
-        address = derive_mxe_pda!()
-    )]
-    pub mxe_account: Account<'info, MXEAccount>,
-    #[account(
-        mut,
-        address = derive_mempool_pda!()
-    )]
-    /// CHECK: mempool_account, checked by the arcium program.
-    pub mempool_account: UncheckedAccount<'info>,
-    #[account(
-        mut,
-        address = derive_execpool_pda!()
-    )]
-    /// CHECK: executing_pool, checked by the arcium program.
-    pub executing_pool: UncheckedAccount<'info>,
-    #[account(
-        mut,
-        address = derive_comp_pda!(computation_offset)
-    )]
-    /// CHECK: computation_account, checked by the arcium program.
-    pub computation_account: UncheckedAccount<'info>,
-    #[account(
-        address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
-    )]
-    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
-    #[account(
-        mut,
-        address = derive_cluster_pda!(mxe_account, ErrorCode::ClusterNotSet)
-    )]
-    pub cluster_account: Account<'info, Cluster>,
-    #[account(
-        mut,
-        address = ARCIUM_FEE_POOL_ACCOUNT_ADDRESS,
-    )]
-    pub pool_account: Account<'info, FeePool>,
-    #[account(
-        address = ARCIUM_CLOCK_ACCOUNT_ADDRESS
-    )]
-    pub clock_account: Account<'info, ClockAccount>,
-    pub system_program: Program<'info, System>,
-    pub arcium_program: Program<'info, Arcium>,
-}
-
-#[callback_accounts("add_together")]
-#[derive(Accounts)]
-pub struct AddTogetherCallback<'info> {
-    pub arcium_program: Program<'info, Arcium>,
-    #[account(
-        address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
-    )]
-    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
-    #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
-    /// CHECK: instructions_sysvar, checked by the account constraint
-    pub instructions_sysvar: AccountInfo<'info>,
-}
-
-#[init_computation_definition_accounts("add_together", payer)]
-#[derive(Accounts)]
-pub struct InitAddTogetherCompDef<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-    #[account(
-        mut,
-        address = derive_mxe_pda!()
-    )]
-    pub mxe_account: Box<Account<'info, MXEAccount>>,
-    #[account(mut)]
-    /// CHECK: comp_def_account, checked by arcium program.
-    /// Can't check it here as it's not initialized yet.
-    pub comp_def_account: UncheckedAccount<'info>,
-    pub arcium_program: Program<'info, Arcium>,
-    pub system_program: Program<'info, System>,
-}
-
-#[event]
-pub struct SumEvent {
-    pub sum: [u8; 32],
-    pub nonce: [u8; 16],
-}
-
 #[error_code]
 pub enum ErrorCode {
     #[msg("The computation was aborted")]
     AbortedComputation,
     #[msg("Cluster not set")]
     ClusterNotSet,
+    #[msg("User position is unhealthy")]
+    UnhealthyPosition,
+    #[msg("Repay amount exceeds borrowed amount")]
+    RepayTooMuch,
 }
